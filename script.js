8489214c932c5be53414525af8488acc1da8f5d6
unknown <aliaashan902@gmail.com>
2023-10-24 16:15:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,170 @@
+
+
+
+function Cup1() {
+    let  cup1rem =  (2000-250)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup1rem;
+    let cup1res = 250/2000 * 100;
+    const perc =  document.getElementById('percentage'); 
+    perc.setAttribute('style', 'height: 12.5%');
+    perc.innerHTML = cup1res + "%";
+    const cupclr = document.getElementById('cup1');
+    cupclr.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup2() {
+    let  cup2rem =  (2000-500)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup2rem;
+
+    let cup2res = 500/2000 * 100;
+    const perc =  document.getElementById('percentage'); 
+    perc.setAttribute('style', 'height: 25%');
+    perc.innerHTML = cup2res + "%";
+
+    const cupclr = document.getElementById('cup1');
+    cupclr.setAttribute('style', 'background-color: #6ab3f8');
+    const cupclr2 = document.getElementById('cup2');
+    cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup3() {
+    let  cup3rem =  (2000-750)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup3rem;
+
+    let cup3res = 750/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 37.5%');
+   perc.innerHTML = cup3res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   
+}
+function Cup4() {
+    let  cup4rem =  (2000-1000)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup4rem;
+
+    let cup4res = 1000/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 50%');
+   perc.innerHTML = cup4res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr4 = document.getElementById('cup4');
+   cupclr4.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup5() {
+    let  cup5rem =  (2000-1250)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup5rem;
+
+    let cup5res = 1250/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 62.5%');
+   perc.innerHTML = cup5res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr4 = document.getElementById('cup4');
+   cupclr4.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr5 = document.getElementById('cup5');
+   cupclr5.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup6() {
+    let  cup6rem =  (2000-1500)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup6rem;
+
+    let cup6res = 1500/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 75%');
+   perc.innerHTML = cup6res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr4 = document.getElementById('cup4');
+   cupclr4.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr5 = document.getElementById('cup5');
+   cupclr5.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr6 = document.getElementById('cup6');
+   cupclr6.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup7() {
+    let  cup7rem =  (2000-1750)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup7rem;
+
+    let cup7res = 1750/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 87.5%');
+   perc.innerHTML = cup7res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr4 = document.getElementById('cup4');
+   cupclr4.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr5 = document.getElementById('cup5');
+   cupclr5.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr6 = document.getElementById('cup6');
+   cupclr6.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr7 = document.getElementById('cup7');
+   cupclr7.setAttribute('style', 'background-color: #6ab3f8');
+}
+function Cup8() {
+    let  cup8rem =  (2000-2000)/1000 + "L";
+    const rem = document.querySelector('.remLtr');
+    rem.setAttribute('style', 'font-size: 30.5px');
+    rem.innerHTML = cup8rem;
+
+    let cup8res = 2000/2000 * 100;
+   const perc =  document.getElementById('percentage'); 
+   perc.setAttribute('style', 'height: 100%');
+   perc.innerHTML = cup8res + "%";
+
+   const cupclr = document.getElementById('cup1');
+   cupclr.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr2 = document.getElementById('cup2');
+   cupclr2.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr3 = document.getElementById('cup3');
+   cupclr3.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr4 = document.getElementById('cup4');
+   cupclr4.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr5 = document.getElementById('cup5');
+   cupclr5.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr6 = document.getElementById('cup6');
+   cupclr6.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr7 = document.getElementById('cup7');
+   cupclr7.setAttribute('style', 'background-color: #6ab3f8');
+   const cupclr8 = document.getElementById('cup8');
+   cupclr8.setAttribute('style', 'background-color: #6ab3f8');
+}
